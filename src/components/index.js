c4a74c328dabb736bee3e68fce5253af1509e23b
ymjung <metalbird0@gmail.com>
2017-01-23 00:05:56 +0900

    set window status

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,10 @@
 import Header from './Header/Header';
 import PostWrapper from './PostWrapper/PostWrapper';
+import Navigate from './Navigate/Navigate';
+import Post from './Post/Post';
+import Comment from './Comment/Comment';
+import CommentList from './CommentList/CommentList';
+import Warning from './Warning/Warning';
 
 export {
     Header,
@@ -7,5 +12,6 @@ export {
     Navigate,
     Post,
     CommentList,
-    Comment
+    Comment,
+    Warning
 };
--- src/services/post.js
+++ src/services/post.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export function getPost(postId) {
-    return axios.get('localhost:3000/open/' + postId);
+    return axios.get('localhost:8080/board/' + postId);
 }
 
 export function getComments(postId) {
-    return axios.get('localhost:3000/open/comment/${postId}')
+    return axios.get('localhost:8080/board/comment/${postId}')
 }

5e8382bb3a0dd9eff671a70ed8948898aeffb23d
ymJung <metalbird0@gmail.com>
2022-12-13 21:53:11 +0900

    dev | 내부 배치 실행을 위한 스케줄링 설정

--- src/main/java/com/metalbird/anyscopeweb/AnyScopeWebApplication.java
+++ src/main/java/com/metalbird/anyscopeweb/AnyScopeWebApplication.java
@@ -2,8 +2,10 @@ package com.metalbird.anyscopeweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AnyScopeWebApplication {
 
 	public static void main(String[] args) {
